916daa7b39d22057cb24166ca4c89b4a76ddbdaf
Cristian Pardo <cpardoca3@gmail.com>
2022-06-10 10:17:43 +0200

    favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import { DefaultSeo } from "next-seo"
+import Head from "next/head"
 import "../css/tailwind.css"
 function MyApp({ Component, pageProps }) {
     return <div className="body">
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
         <DefaultSeo
         defaultTitle="Dragones y Naves Espaciales"
         description="Blog de ciencia ficción y fantasía donde exploramos todos los mundos posibles y hablamos sobre nuestra experiencia porque el espacio está lleno de dragones."
