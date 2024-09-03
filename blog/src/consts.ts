7738366faad4315bfe7203a2b501ef490bc4e997
VISUAL-LIMES\cristian.pardo <cristian.pardo@visual-limes.com>
2024-09-03 10:08:21 +0200

    favicon and seo description

--- blog/src/consts.ts
+++ blog/src/consts.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'BelicStorm Blog';
-export const SITE_TAGLINE = 'Minimalist Starter for Developer Blog & Portfolio Website'; // site tagline
-export const SITE_DESCRIPTION = 'Minimalist Starter for Developer Blog & Portfolio Website'; // site meta description for SEO
+export const SITE_TAGLINE = 'Minimalist Blog & Portfolio Website'; // site tagline
+export const SITE_DESCRIPTION = 'Minimalist Blog & Portfolio Website'; // site meta description for SEO
